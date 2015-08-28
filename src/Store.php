41ca36574b2f926bd8b9ba26e38fefb3d831ef1b
Ben Spenard <bdspen@gmail.com>
2015-08-28 10:58:10 -0700

    write methods for save getall and delete all, add teardown to test.

--- src/Store.php
+++ src/Store.php
@@ -22,6 +22,29 @@
 		{
 			return $this->id;
 		}
-		
+		function save()
+		{
+			$GLOBALS['DB']->exec("INSERT INTO stores (names)
+			VALUES ('{$this->getName()}');");
+			$this->id = $GLOBALS['DB']->lastInsertId();
+			$GLOBALS['DB']->exec("INSERT INTO copies (store_id) VALUES ({$this->getId()})");
+		}
+		static function deleteAll()
+		{
+			$GLOBALS['DB']->exec("DELETE FROM stores;");
+			$GLOBALS['DB']->exec("DELETE FROM stores_authors;");
+		}
+		static function getAll()
+		{
+			$returned_stores = $GLOBALS['DB']->query("SELECT * FROM stores;");
+			$stores = array();
+			foreach($returned_stores as $store) {
+			$name = $store['names'];
+			$id = $store['id'];
+			$new_store = new Store ($name, $id);
+			array_push($stores, $new_store);
+		}
+			return $stores;
+		}
 	}
  ?>
--- tests/StoreTest.php
+++ tests/StoreTest.php
@@ -13,81 +13,86 @@
 
 	class StoreTest extends PHPUnit_Framework_TestCase
 	{
-	  	  function test_getName()
-      {
-        //Arrange
-        $name = "Shoe One";
-        $id = 1;
-        $test_store = new Store ($name, $id);
-        //Act
-        $result = $test_store->getName();
-        //Assert
-        $this->assertEquals ($name, $result);
-      }
-      function test_setName()
-      {
-        //Arrange
-        $name = "Shoe One";
-        $id = 1;
-        $test_store = new Store ($name, $id);
-        //Act
-        $test_store->setName("Source Shoe");
-        $result = $test_store->getName();
-        //Assert
-        $this->assertEquals($result, "Source Shoe");
-      }
-      function test_getId()
-      {
-        //Arrange
-        $id = 1;
-        $name = "Shoe One";
-        $test_store = new Store ($name, $id);
-        //Act
-        $result = $test_store->getId();
-        //Assert
-        $this->assertEquals(1, $result);
-      }
-	  function test_save()
-	{
-		//Arrange
-		$name = "Shoe One";
-		$id = 1;
-		$test_store = new Store($name, $id);
-		//Act
-		$test_store->save();
-		//Assert
-		$result = Store::getAll();
-		$this->assertEquals($result[0], $test_store);
-			}
-	function test_getAll()
-	{
-		//arrange
-		$name = "Shoe Shoe";
-		$id = 1;
-		$test_store1 = new Store($name, $id);
-		$test_store1->save();
-		$name2 = "Blue Shoe";
-		$id2 = 2;
-		$test_store2 = new Store($name2, $id2);
-		$test_store2->save();
-		//act
-		$result = Store::getAll();
-		//assert
-		$this->assertEquals([$test_store1, $test_store2], $result);
-	}
-	function test_deleteAll()
-	{
-		$name = "Shoe Shoe";
-		$id = 1;
-		$test_store1 = new Store($name, $id);
-		$test_store1->save();
-		$name2 = "Blue Shoe";
-		$id2 = 2;
-		$test_store2 = new Store($name2, $id2);
-		$test_store2->save();
+		protected function tearDown()
+		{
 		Store::deleteAll();
-		$result = Store::getAll();
-		$this->assertEquals([], $result);
-	}
+		}
+
+		function test_getName()
+		{
+	        //Arrange
+	        $name = "Shoe One";
+	        $id = 1;
+	        $test_store = new Store ($name, $id);
+	        //Act
+	        $result = $test_store->getName();
+	        //Assert
+	        $this->assertEquals ($name, $result);
+		}
+		function test_setName()
+		{
+	        //Arrange
+	        $name = "Shoe One";
+	        $id = 1;
+	        $test_store = new Store ($name, $id);
+	        //Act
+	        $test_store->setName("Source Shoe");
+	        $result = $test_store->getName();
+	        //Assert
+	        $this->assertEquals($result, "Source Shoe");
+	 	}
+		function test_getId()
+		{
+	        //Arrange
+	        $id = 1;
+	        $name = "Shoe One";
+	        $test_store = new Store ($name, $id);
+	        //Act
+	        $result = $test_store->getId();
+	        //Assert
+	        $this->assertEquals(1, $result);
+	    }
+		function test_save()
+		{
+			//Arrange
+			$name = "Shoe One";
+			$id = 1;
+			$test_store = new Store($name, $id);
+			//Act
+			$test_store->save();
+			//Assert
+			$result = Store::getAll();
+			$this->assertEquals($result[0], $test_store);
+		}
+		function test_getAll()
+		{
+			//arrange
+			$name = "Shoe Shoe";
+			$id = 1;
+			$test_store1 = new Store($name, $id);
+			$test_store1->save();
+			$name2 = "Blue Shoe";
+			$id2 = 2;
+			$test_store2 = new Store($name2, $id2);
+			$test_store2->save();
+			//act
+			$result = Store::getAll();
+			//assert
+			$this->assertEquals([$test_store1, $test_store2], $result);
+		}
+		function test_deleteAll()
+		{
+			$name = "Shoe Shoe";
+			$id = 1;
+			$test_store1 = new Store($name, $id);
+			$test_store1->save();
+			$name2 = "Blue Shoe";
+			$id2 = 2;
+			$test_store2 = new Store($name2, $id2);
+			$test_store2->save();
+			Store::deleteAll();
+			$result = Store::getAll();
+			$this->assertEquals([], $result);
+		}
 	}
 ?>
